a837a9eb3d809dadfdaae98087cdd31dd8f8de13
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-11-19 03:48:21 +0200

    deploy fixed

--- src/components/GoodsGallery.jsx
+++ src/components/GoodsGallery.jsx
@@ -4,38 +4,38 @@ function GoodsGallery() {
     const products = [
       {
         id: 1,
-        image: "/img/products/carrot.jpg",
+        image: "./img/products/carrot.jpg",
         name: "Carrot",
         price: 15.50,
       },
       {
         id: 2,
-        image: "/img/products/broccoli.jpg",
+        image: "./img/products/broccoli.jpg",
         name: "Broccoli",
         price: 42.00,
       },
       {
         id: 3,
-        image: "/img/products/eggplant.jpg",
+        image: "./img/products/eggplant.jpg",
         name: "Eggplant",
         price: 78.30,
       },
       {
         id: 4,
-        image: "/img/products/zucchini.jpg",
+        image: "./img/products/zucchini.jpg",
         name: "Zucchini",
         price: 23.75,
       },
       {
         id: 5,
-        image: "/img/products/kale.jpg",
+        image: "./img/products/kale.jpg",
         name: "Kale",
         price: 99.99,
       },
       {
         id: 6,
         
-        image: "/img/products/radish.jpg",
+        image: "./img/products/radish.jpg",
         name: "Radish",
         price: 12.40,
       },
